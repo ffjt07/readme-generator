33d1124f609052a1a017cb70f2bf2fa829911c65
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-09-23 23:50:53 -0600

    debugged readme and added github link, added line breaks and functions for creating sections

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# readme-generator
+# Title
+
+  ## Description
+  
+  Project
+
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+  Use it
+
+  ## Usage
+  
+  use it
+
+
+  ## Credits
+
+  Me
+  
+  ## License
+  MIT
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  [https://opensource.org/licenses/MIT])
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  ## Contributing
+  
+  Cash MONEY!
+
+  ## Tests
+
+  use it
+
+  ## Questions? 
+
+  ffjt07
+
+  
--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs')
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "What is the description of the project?",
+        name: "descript",
+      },
+      {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "install",
+      },
+      {
+        type: "input",
+        message: "Who contributed to the project?",
+        name: "users",
+      },
+      {
+        type: "input",
+        message: "Instructions for use",
+        name: "instruct",
+      },
+      {
+        type: "input",
+        message: "What is the Github username for the contributor?",
+        name: "git",
+      },
+      {
+        type: "list",
+        message: "Which License?",
+        name: "license",
+        choices: ["MIT", "GNU GPL V2", "APACHE 2.0", "None"]
+      },
+      {
+        type: "input",
+        message: "How to contribute",
+        name: "giveme$",
+      },
+      {
+        type: "input",
+        message: "How to test the app",
+        name: "test",
+      },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile("README.md", generateMarkdown(data), (err) =>
+    err ? console.log(err) : console.log("Successfully created README!")
+    )
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => writeToFile(data))
+}
 
 // Function call to initialize app
 init();
